9a942c5eaaa772f257dbc35446f6d825af102741
Fazan4ik <91279825+Fazan4ik@users.noreply.github.com>
2023-07-25 15:55:16 +0300

    Update SQLHW20.07.2023.sql

--- SQLHW20.07.2023.sql
+++ SQLHW20.07.2023.sql
@@ -1,23 +1,23 @@
--- Задание 1
+-- Р—Р°РґР°РЅРёРµ 1
 /*
 SELECT p.name AS 'product', c.name AS 'category', s.name AS 'deliverer'
 FROM Product AS p
 JOIN Category AS c ON c.id = p.id_category
 JOIN Delivery AS d ON p.id = d.id_product
 JOIN Supplier AS s ON s.id = d.id_supplier
-WHERE s.name IN ('ООО Паньки', 'ООО Какие люди') */
+WHERE s.name IN ('РћРћРћ РџР°РЅСЊРєРё', 'РћРћРћ РљР°РєРёРµ Р»СЋРґРё') */
 
--- Задание 2
+-- Р—Р°РґР°РЅРёРµ 2
 /*
-SELECT p.name AS 'Product', c.name AS 'category', s.name AS 'поставщик', pr.name AS 'producer'
+SELECT p.name AS 'Product', c.name AS 'category', s.name AS 'deliverer', pr.name AS 'producer'
 FROM Product AS p
 JOIN Delivery AS d ON p.id = d.id_product
 JOIN Supplier AS s ON s.id = d.id_supplier
 JOIN Producer as pr ON pr.id = p.id_producer
 JOIN Category AS c ON c.id = p.id_category
-WHERE pr.name NOT LIKE '%[АКМ]%' AND c.name != 'Крупы' */
+WHERE pr.name NOT LIKE '%[РђРљРњ]%' AND c.name != 'РљСЂСѓРїС‹' */
 
--- Задание 3
+-- Р—Р°РґР°РЅРёРµ 3
 /*
 SELECT p.name AS 'Product', c.name AS 'category', s.name AS 'delivery' , d.price,
 d.date_of_delivery, co.name AS 'country of delivery'
@@ -30,10 +30,10 @@ JOIN Address AS a ON a.id = pr.id_address
 JOIN City AS ci ON ci.id = a.id_city
 JOIN Region AS r ON r.id = ci.id_region
 JOIN Country AS co ON co.id = r.id_country
-WHERE co.name NOT IN ('Украина', 'Молдова', 'Польша') AND d.price < 2000
+WHERE co.name NOT IN ('РЈРєСЂР°РёРЅР°', 'РњРѕР»РґРѕРІР°', 'РџРѕР»СЊС€Р°') AND d.price < 2000
 AND d.date_of_delivery BETWEEN '2023-05-10' AND GETDATE() */
 
--- Задание 4
+-- Р—Р°РґР°РЅРёРµ 4
 /*
 SELECT p.name, c.name, s.quantity, sup.name, pr.name
 FROM Product AS p
@@ -42,9 +42,9 @@ JOIN Sale AS s ON p.id = s.id_product
 JOIN Delivery AS d ON p.id = d.id_product
 JOIN Supplier AS sup ON sup.id = d.id_supplier
 JOIN Producer AS pr ON pr.id = p.id_producer
-WHERE c.name IN ('Кондитерские изделия', 'Безалкогольные напитки') AND s.quantity > 100 */
+WHERE c.name IN ('РљРѕРЅРґРёС‚РµСЂСЃРєРёРµ РёР·РґРµР»РёСЏ', 'Р‘РµР·Р°Р»РєРѕРіРѕР»СЊРЅС‹Рµ РЅР°РїРёС‚РєРё') AND s.quantity > 100 */
 
--- Задание 5
+-- Р—Р°РґР°РЅРёРµ 5
 /*
 SELECT p.name AS 'Product', c.name AS 'category', s.name AS 'deliverer',
 d.date_of_delivery, d.price AS 'price delivery'
@@ -52,10 +52,11 @@ FROM Product AS p
 JOIN Delivery AS d ON p.id = d.id_product
 JOIN Supplier AS s ON s.id = d.id_supplier
 JOIN Category AS c ON c.id = p.id_category
-WHERE s.name IN ('ТОВ Снабжение', 'Супер закупка', 'Фрукт постав')
+WHERE s.name IN ('РўРћР’ РЎРЅР°Р±Р¶РµРЅРёРµ', 'РЎСѓРїРµСЂ Р·Р°РєСѓРїРєР°', 'Р¤СЂСѓРєС‚ РїРѕСЃС‚Р°РІ')
 ORDER BY p.name */
 
--- Задание 6
+-- Р—Р°РґР°РЅРёРµ 6
+/*
 SELECT p.name AS 'Product', pr.name AS 'Producer', (co.name + ', ' + c.name + ', ' + a.street) AS 'Full adress',
 cat.name AS 'Category', s.date_of_sale AS 'Date of sell', s.price AS 'Sell price'
 FROM Product AS p
@@ -66,7 +67,7 @@ JOIN Region AS r ON r.id = c.id_region
 JOIN Country AS co ON co.id = r.id_country
 JOIN Category AS cat ON cat.id = p.id_category
 JOIN Sale AS s ON p.id = s.id_product
-WHERE pr.name NOT IN ('ТОВ Снабжение', 'Супер закупка')
-ORDER BY s.price DESC
+WHERE pr.name NOT IN ('РўРћР’ РЎРЅР°Р±Р¶РµРЅРёРµ', 'РЎСѓРїРµСЂ Р·Р°РєСѓРїРєР°')
+ORDER BY s.price DESC */
   
   
