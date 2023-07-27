349dd56220a6ac6270a8b360f3730b738dfb56ab
Fazan4ik <91279825+Fazan4ik@users.noreply.github.com>
2023-07-27 16:09:32 +0300

    Update SQLHW21.07.2023.sql

--- SQLHW21.07.2023.sql
+++ SQLHW21.07.2023.sql
@@ -1,10 +1,10 @@
--- Задание 1
+-- Р—Р°РґР°РЅРёРµ 1
 /*
 SELECT p.name AS 'Product', pr.name AS 'Producer'
 FROM Product AS p
 RIGHT JOIN Producer AS pr ON pr.id = p.id_producer */
 
--- Задание 2 
+-- Р—Р°РґР°РЅРёРµ 2 
 /*
 SELECT p.name AS 'Product', s.name AS 'supplier', d.date_of_delivery AS 'date of delivery', d.id_supplier
 FROM Product AS p
@@ -12,7 +12,7 @@ JOIN Delivery AS d ON p.id = d.id_product
 RIGHT JOIN Supplier AS s ON s.id = d.id_supplier
 ORDER BY d.id_supplier */
 
--- Задание 3
+-- Р—Р°РґР°РЅРёРµ 3
 /*
 SELECT name
 FROM Category
@@ -21,9 +21,9 @@ SELECT DISTINCT c.name
 FROM Product AS p
 JOIN Category AS c ON c.id = p.id_category
 JOIN Producer AS pr ON pr.id = p.id_producer
-WHERE pr.name = 'Нли Ю' */
+WHERE pr.name = 'РќР»Рё Р®' */
 
--- Задание 4
+-- Р—Р°РґР°РЅРёРµ 4
 /*
 SELECT name
 FROM Producer
@@ -32,4 +32,4 @@ SELECT pr.name
 FROM Product AS p
 JOIN Category AS c ON c.id = p.id_category
 JOIN Producer AS pr ON pr.id = p.id_producer
-WHERE c.name = 'Молочные' */
+WHERE c.name = 'РњРѕР»РѕС‡РЅС‹Рµ' */
