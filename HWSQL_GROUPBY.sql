15fe3a1fad08b31f4eb182d4fffb5b743b2d5cf3
Fazan4ik <91279825+Fazan4ik@users.noreply.github.com>
2023-08-09 15:50:05 +0300

    Update HWSQL_GROUPBY.sql

--- HWSQL_GROUPBY.sql
+++ HWSQL_GROUPBY.sql
@@ -1,11 +1,11 @@
--- çàäàíèå 1
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 1
 /*
 SELECT p.name, MIN(s.price) AS 'Min sell price', COUNT(s.price) AS 'Num sells'
 FROM Product AS p
 JOIN Sale AS s ON p.id = s.id_product
 GROUP BY p.name */
 
--- çàäàíèå 2
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 2
 /*
 SELECT p.name, AVG(s.price) AS 'avg sell price'
 FROM Product AS p
@@ -13,7 +13,7 @@ JOIN Sale AS s ON p.id = s.id_product
 GROUP BY p.name
 HAVING AVG(s.price) > 50 */
 
--- çàäàíèå 3
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 3
 /*
 SELECT c.name AS 'category', COUNT(p.name) AS 'quantity'
 FROM Product AS p
@@ -22,16 +22,16 @@ JOIN Delivery AS d ON p.id = d.id_product
 GROUP BY c.name
 HAVING AVG(d.price) > 100 */
 
--- çàäàíèå 4
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 4
 /*
 SELECT p.name AS 'Name', SUM(s.price * s.quantity) AS 'sum sells'
 FROM Product AS p
 JOIN Category AS c ON c.id = p.id_category
 JOIN Sale AS s ON p.id = s.id_product
-WHERE c.name IN ('Ôðóêòû', 'Êîíôåòû')
+WHERE c.name IN ('Ð¤Ñ€ÑƒÐºÑ‚Ñ‹', 'ÐšÐ¾Ð½Ñ„ÐµÑ‚Ñ‹')
 GROUP BY p.name */
 
--- çàäàíèå 5
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 5
 /* 
 SELECT s.name, MIN(d.price)
 FROM Product AS p
@@ -41,7 +41,7 @@ WHERE d.date_of_delivery BETWEEN DATEADD(MONTH, -1, GETDATE()) AND GETDATE()
 GROUP BY s.name
 ORDER BY 2 */
 
--- çàäàíèå 6
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 6
 /*
 SELECT pr.name AS 'manufacter', COUNT(p.name) AS 'qunantity products', (co.name + ', ' + r.name + ', ' + c.name + ', ' + a.street) AS 'full adress'
 FROM Product AS p
@@ -54,7 +54,7 @@ JOIN Sale AS s ON p.id = s.id_product
 GROUP BY pr.name, (co.name + ', ' + r.name + ', ' + c.name + ', ' + a.street)
 HAVING SUM(s.price) BETWEEN 100 AND 200 */
 
--- çàäàíèå 7
+-- Ð·Ð°Ð´Ð°Ð½Ð¸Ðµ 7
 /*
 SELECT TOP 1 c.name AS 'category'
 FROM Product AS p
@@ -62,13 +62,13 @@ JOIN Category AS c ON c.id = p.id_category
 GROUP BY c.name
 ORDER BY COUNT(p.name) */
 
--- Çàäàíèå 8
+-- Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 8
 /*
 SELECT c.name AS 'category', COUNT(p.name) AS 'quantity'
 FROM Product AS p
 JOIN Category AS c ON c.id = p.id_category
 JOIN Delivery AS d ON p.id = d.id_product
 JOIN Supplier AS s ON s.id = d.id_supplier
-WHERE s.name IN ('Þæ ïîñòàâêà', 'ÒÎÂ Ñíàáæåíèå', 'Ôðóêò ïîñòàâ')
+WHERE s.name IN ('Ð®Ð¶ Ð¿Ð¾ÑÑ‚Ð°Ð²ÐºÐ°', 'Ð¢ÐžÐ’ Ð¡Ð½Ð°Ð±Ð¶ÐµÐ½Ð¸Ðµ', 'Ð¤Ñ€ÑƒÐºÑ‚ Ð¿Ð¾ÑÑ‚Ð°Ð²')
 GROUP BY c.name
 HAVING SUM(d.price) > 400 */
